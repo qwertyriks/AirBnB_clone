ba7cf62019ec82acd293ec2d958d2e4a95def0be
Abrhaley Gebreslassie <38569122+Abrish-seng@users.noreply.github.com>
2024-03-11 02:35:04 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-'''Defines the User subclass.'''
+"""Write the User subclass."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    '''Represent a User.
+   """Represent a User.
 
     Attributes:
         email (str): The email of the user.
         password (str): The password of the user.
         first_name (str): The first name of the user.
         last_name (str): The last name of the user.
-    '''
+    """
 
     email = ""
     password = ""
