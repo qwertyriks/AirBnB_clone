521bae489fc7effb39e3d11ba67d03fc15e3e2ed
Abrhaley Gebreslassie <38569122+Abrish-seng@users.noreply.github.com>
2024-03-11 01:18:10 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-'''Defines unit tests for models/base_model.py.
+"""Defines unit tests for models/base_model.py.
 
-Unittest classes:
-    TestBaseModel_instantiation
-    TestBaseModel_save
-    TestBaseModel_to_dict
-'''
+"""
 import os
 import models
 import unittest
@@ -15,7 +11,7 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel_instantiation(unittest.TestCase):
-    '''Unit tests for testing instantiation of the BaseModel class.'''
+    """Unit tests for testing instantiation of the BaseModel class."""
 
     def test_no_args_instantiates(self):
         self.assertEqual(BaseModel, type(BaseModel()))
@@ -87,7 +83,7 @@ class TestBaseModel_instantiation(unittest.TestCase):
 
 
 class TestBaseModel_save(unittest.TestCase):
-    '''Unit tests for testing save method of the BaseModel class.'''
+    """Unit tests for testing save method of the BaseModel class."""
 
     @classmethod
     def setUp(self):
@@ -139,7 +135,7 @@ class TestBaseModel_save(unittest.TestCase):
 
 
 class TestBaseModel_to_dict(unittest.TestCase):
-    '''Unit tests for testing to_dict method of the BaseModel class.'''
+    """Unit tests for testing to_dict method of the BaseModel class."""
 
     def test_to_dict_type(self):
         bm = BaseModel()
